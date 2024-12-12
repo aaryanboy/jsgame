6538ead8340345deb56ac4f1c68d644cf379114b
aaryanboy <aaryanboy12@gmail.com>
2024-12-12 06:54:13 +0545

    just one room for now

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+//inside room
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+      I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+      Here is my <a href="https://github.com/aaryanboy" target="_blank">Github</a>!`,
+  tv: `That's my TV. I've been watching saucy vdos `,
+  bed: `This where I sleep. `,
+  resume: `This is my desk and on it is my resume`,
+  "sofa-table": `its just a table`,
+  "cs-degree": `its a fake degree`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves.`,
+  exit: `If you want to exit  room, just close the tab.`,
+};
---
+++ src/utils.js
@@ -0,0 +1,44 @@
+export function displayDialogue(text, onDisplayEnd) {
+  const dialogueUI = document.getElementById("textbox-container");
+  const dialogue = document.getElementById("dialogue");
+
+  dialogueUI.style.display = "block";
+  let index = 0;
+  let currentText = ""; //text to append in page
+  const intervalRef = setInterval(() => {
+    if (index < text.length) {
+      currentText += text[index];
+      dialogue.innerHTML = currentText; //can get hacked if accepte user input
+      index++;
+      return;
+    }
+
+    clearInterval(intervalRef);
+  }, 5); //5 sec
+  const closeBtn = document.getElementById("close");
+
+  function onCloseBtnClick() {
+    onDisplayEnd();
+    dialogueUI.style.display = "none"; //diaglog is invisible and empty,, clear intervel if not done
+    dialogue.innerHTML = "";
+    clearInterval(intervalRef);
+    closeBtn.removeEventListener("click", onCloseBtnClick);
+  }
+
+  closeBtn.addEventListener("click", onCloseBtnClick);
+
+  addEventListener("keypress", (key) => {
+    if (key.code === "Enter") {
+      closeBtn.click();
+    }
+  });
+}
+
+export function setCamScale(k) {
+  const resizeFactor = k.width() / k.height();
+  if (resizeFactor < 1) {
+    k.camScale(k.vec2(1));
+  } else {
+    k.camScale(k.vec2(1.5));
+  }
+}
